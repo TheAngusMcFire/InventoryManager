eeae5461b47b25dfbf0f5f81b37be0ef8537cf64
Christian Rieger <christian.rieger@student.tugraz.at>
2018-09-18 19:06:30 +0200

    removed init of the label maker

--- InventoryManager/Form1.cs
+++ InventoryManager/Form1.cs
@@ -20,13 +20,24 @@ namespace InventoryManager
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            lbl_handler = new LabelMakerHandler();
-            lbl_handler.init();
+            //lbl_handler = new LabelMakerHandler();
+            //lbl_handler.init();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (lbl_handler == null)
+            {
+                printError("Error printer needs to be initialized");
+                return;
+            }
+
             lbl_handler.printLabel(richTextBox1.Text);
+        }
+
+        private void printError(string msg, Exception exc = null)
+        {
+            MessageBox.Show($"{msg} \n {exc}");
         }
     }
 }
